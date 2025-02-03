31126412a723332e9090e8641629c1223a1c76b8
MURAMASA2470 <keisuke226214@gmail.com>
2025-02-03 22:37:02 +0900

    DB登録時のエラー制御周りを追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        
+        // 存在しないカラムに値を入れようとするとエラーが起きるように
+        Model::preventAccessingMissingAttributes(
+            !$this->app->isProduction()
+        );
+
+        // 存在しないカラムに値をセットして保存するとエラーになるように
+        Model::preventSilentlyDiscardingAttributes();
+        
+        // N + 1が発生したらエラーになるように
+        Model::preventLazyLoading(
+                !$this->app->isProduction()
+        );
+
     }
 
     /**
